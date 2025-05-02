c87a3646750214b61deb9d781fc41cf3e80ef954
Simon <simonwarui93@gmail.com>
2025-05-02 19:45:01 +0300

    assignment

---
+++ src/components/Article.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+    let readTime = "";
+    
+    if (minutes < 30) {
+      const cups = Math.ceil(minutes / 5);
+      readTime = "☕️".repeat(cups);
+    } else {
+      const boxes = Math.ceil(minutes / 10);
+      readTime = "🍱".repeat(boxes);
+    }
+  
+    return (
+      <article>
+        <h3>{title}</h3>
+        <small>{date} • {readTime} {minutes} min read</small>
+        <p>{preview}</p>
+      </article>
+    );
+}
+
+export default Article;
